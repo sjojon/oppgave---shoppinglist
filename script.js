6710cb3fa8a1758ac1f85fac5849a68fcdf6384b
sjojon <90796535+sjojon@users.noreply.github.com>
2023-02-11 17:04:54 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+//model
+let list = [];
+let onChange;
+
+
+//view
+view()
+function view() {
+    let html = /*html*/ `
+    <h1>Handleliste:</h1>
+    <input type="text" onchange="addItem(this.value)">
+    <button onclick="addItem">Legg til</button>
+    `;
+    for (i = 0; list.length > i; i++) {
+        html += /*html*/`<li> ${list[i]}</li><br>
+        <button onclick="removeItem(${i})">Slett</button>
+        <button onclick="changeItem">Forandre</button>
+        <input type="text" onchange="changeItem(this.value, ${i})">
+        `
+
+    }
+    document.getElementById('app').innerHTML = html;
+}
+
+//controller
+function addItem(liste) {
+    list.push(liste);
+    view()
+}
+
+function removeItem(liste) {
+    list.splice(liste, 1);
+    view()
+}
+
+function changeItem(onChange, index) {
+    list[index] = onChange;
+    view()
+}
